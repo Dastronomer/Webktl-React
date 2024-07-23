c7c0fd3cb7b1931229e87bc2d710c7b62deb13a6
ashnakashima <nakashlyn@gmail.com>
2024-07-23 15:30:42 -0700

    drop down button with optionKeyword
    KeywordDropdownButton is now able to take a keyword and grab their value and use its contents as the dropdown options

--- src/components/KeywordDropdownButton.js
+++ src/components/KeywordDropdownButton.js
@@ -1,12 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import {useWebSocket1} from "../context/WebSocketProviders";
 
-function KeywordDropdownButton({label, keyword, options, makeConfirm}) {
+function KeywordDropdownButton({label, keyword, options, makeConfirm, optionsKeyword}) {
     const {sendMessage, messages} = useWebSocket1();
-
-
     const [selectedOption, setSelectedOption] = useState('');
     const [initialOption, setInitialOption] = useState('');
+    const [optionsArray, setOptionsArray] = useState(Array.isArray(options) ? options : []);
 
     useEffect(() => {
         const message = messages.find((msg) => msg.key === keyword);
@@ -16,6 +15,19 @@ function KeywordDropdownButton({label, keyword, options, makeConfirm}) {
         }
     }, [keyword, messages]);
 
+    useEffect(() => {
+        if (optionsKeyword) {
+            const message = messages.find((msg) => msg.key === optionsKeyword);
+            if (message) {
+                const messageValue = message.value;
+                setOptionsArray(messageValue.split(" "));
+            } else {
+                setOptionsArray(Array.isArray(options) ? options : []);
+            }
+        } else {
+            setOptionsArray(Array.isArray(options) ? options : []);
+        }
+    }, [optionsKeyword, messages, options]);
 
     const keyArray = keyword.split(".");
     let key = keyArray[1];
@@ -31,6 +43,7 @@ function KeywordDropdownButton({label, keyword, options, makeConfirm}) {
 
     return (
         <div  style={{fontSize:10, display:'block', margin:3}}>
+
             <label>
                 {label ? label : `${key}: ` }
             <select
@@ -39,7 +52,7 @@ function KeywordDropdownButton({label, keyword, options, makeConfirm}) {
                 id={`${keyword}-dropdown`}
                 value={selectedOption}
             >
-                {options.map((value, index) => {
+                {optionsArray.map((value, index) => {
                     return(
                     <option key={index} value={value} >{value}</option>
                     )
